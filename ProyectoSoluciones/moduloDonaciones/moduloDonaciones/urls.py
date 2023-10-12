9d0fffd3f340d5b8a5c0ae4baec2667f991dc6d8
ChristianPDAG <christian.perezdearce@gmail.com>
2023-10-12 13:20:32 -0300

    jaja

--- ProyectoSoluciones/moduloDonaciones/moduloDonaciones/urls.py
+++ ProyectoSoluciones/moduloDonaciones/moduloDonaciones/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from moduloDonacionesApp.views import *
+from moduloDonacionesApp.views import renderFormDon
+from moduloDonacionesApp.views import renderFormUs
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- ProyectoSoluciones/moduloDonaciones/moduloDonacionesApp/views.py
+++ ProyectoSoluciones/moduloDonaciones/moduloDonacionesApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def renderFormDon(request):
-    return render(request,'template/Form_Donacion.html')
+    return render(request,'template/form_donacion.html')
 
 def renderFormUs(request):
-    return render (request,'template/Form_Usuarios')
+    return render(request,'template/Form_Usuarios.html')
