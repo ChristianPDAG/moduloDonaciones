06481585cc7994d08b7b90d8e8cc585dce105121
ChristianPDAG <christian.perezdearce@gmail.com>
2023-10-12 12:37:40 -0300

    avance backend views,urls and settings

--- ProyectoSoluciones/moduloDonaciones/moduloDonaciones/urls.py
+++ ProyectoSoluciones/moduloDonaciones/moduloDonaciones/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from moduloDonacionesApp.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("formUsuario/", renderFormUs),
+    path("formDonacion/", renderFormDon)
+
 ]
--- ProyectoSoluciones/moduloDonaciones/moduloDonacionesApp/views.py
+++ ProyectoSoluciones/moduloDonaciones/moduloDonacionesApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def renderFormDon(request):
+    return render(request,'template/Form_Donacion.html')
+
+def renderFormUs(request):
+    return render (request,'template/Form_Usuarios')
